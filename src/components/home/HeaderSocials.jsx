82aee4fe9eb3bff6c2552691ba7db69ed49c9cf2
ximemunoz <92742110+ximemunoz@users.noreply.github.com>
2026-04-27 15:25:18 -0600

    github icon

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const HeaderSocials = () => {
   return (
@@ -14,6 +15,9 @@ const HeaderSocials = () => {
         </a>
         <a href="https://www.linkedin.com/in/ximena-munoz-b52801306" className="home__social-link" target="_blank">
             <LinkedInIcon/>
+        </a>
+        <a href="https://github.com/ximemunoz" className="home__social-link" target="_blank">
+            <GitHubIcon/>
         </a>
     </div>
   )
